c90dcd41d882b8254d889fdce45e8e7a79c81366
Ledsonvieira <54151999+Ledsonvieira@users.noreply.github.com>
2019-08-15 15:05:37 -0300

    Média da escola com apenas 2 unidades

---
+++ Gibbynho.py
@@ -0,0 +1,11 @@
+nome = (input('Digite seu nome: '))
+nota1 = float(input('Digite sua nota da 1° Unidade: '))
+nota2 = float(input('Digite sua nota da 2° Unidade: '))
+nota3 = float(input('Digite sua nota da 3° Unidade: '))
+nota4 = float(input('Digite sua nota da 4° Unidade: '))
+result = nota1+nota2+nota3+nota4
+result2 = 24-result
+result3 = result2/2
+print(f'Você é burro {nome} e precisa tirar, {result3} na 3° Unidade e {result3} na 4° Unidade.')
+
+
